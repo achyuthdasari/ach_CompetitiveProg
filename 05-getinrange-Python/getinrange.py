3995db13b30ca2c0047aafdd0c54399b7129e0a8
achyuthdasari <achyuthdasari.24@gmail.com>
2021-08-13 07:43:30 +0530

    done

---
+++ 05-getinrange-Python/getinrange.py
@@ -0,0 +1,14 @@
+# Write the function getInRange(x, bound1, bound2) which takes 3 int values
+# x, bound1, and bound2, where bound1 is not necessarily less than bound2. 
+# If x is between the two bounds, just return it unmodified. Otherwise, 
+# if x is less than the lower bound, return the lower bound, 
+# or if x is greater than the upper bound, return the upper bound.
+
+def fun_getinrange(x, bound1, bound2):
+	# your code goes here
+	maxi , mini =max(bound1, bound2),  min(bound1, bound2)
+	if x>=maxi: return maxi
+	elif x<=mini: return mini
+	else: return x
+
+	return -1
