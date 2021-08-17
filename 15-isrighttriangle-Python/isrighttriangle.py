67b772dcd6c137d322520d9b4448dc7fa1ab1f7d
achyuthdasari <achyuthdasari.24@gmail.com>
2021-08-17 09:48:39 +0530

    solved

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,11 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	d1=(x1-x2)**2+(y1-y2)**2
+	d2=(x3-x2)**2+(y3-y2)**2
+	d3=(x1-x3)**2+(y1-y3)**2
+
+	if d1==(d2+d3) or d2==(d3+d1) or (d3==d1+d2):
+		return True
+	else:
+		return False
