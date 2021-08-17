96fd36c5592bdc71844220d3901c6cd2d71c28ed
achyuthdasari <achyuthdasari.24@gmail.com>
2021-08-17 10:45:37 +0530

    solved

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,5 +7,20 @@
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+	flag=0
+	if n<0:
+		flag=1
+	n=str(abs(n))
+	if k<len(n):
+		ans=n[:len(n)-k-1]+str(d)+n[len(n)-k:]
+	else:
+		ans=str(d)+'0'*(k-len(n))+n
+	if flag==0:
+		return int(ans)
+	else:
+		return int(ans)*-1
+
+print(fun_set_kth_digit(586, 4, 9))
+	
+
 
