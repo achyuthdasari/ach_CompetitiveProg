50118d0c5ccc833384ea02316ce533e03ff1be69
achyuthdasari <achyuthdasari.24@gmail.com>
2021-08-19 15:24:43 +0530

    solved

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -19,4 +19,18 @@
 
 def ishappynumber(n):
 	# your code goes here
-	pass
+	lst=[]
+	while(True):
+		sum=0
+		while(n>0):
+			sum+=(n%10)**2
+			n=n//10
+		# print(sum,"----------")
+		if sum==1:
+			return True
+		else:
+			if sum in lst:
+				return False
+			# print(sum)
+			lst.append(sum)
+			n=sum
