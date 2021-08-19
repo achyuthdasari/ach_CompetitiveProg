4831fa306333b41e85823ed7b47f048f584d529d
achyuthdasari <achyuthdasari.24@gmail.com>
2021-08-19 11:26:00 +0530

    solved

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,7 +5,22 @@
 
 
 def fun_matrixmultiply(m1, m2):
-    return None
+
+    r1,r2,c1,c2=len(m1),len(m2),len(m1[0]),len(m2[0])
+
+    if c1!=r2: return None
+
+    result=[[0]*c2 for p in range(r1)]
+    for i in range(r1):
+        for j in range(c2):
+            ans=0
+            for k in range(c1):
+                ans+=m1[i][k]*m2[k][j]
+            result[i][j]=ans
+    
+    return result
+
+
 
 
 
