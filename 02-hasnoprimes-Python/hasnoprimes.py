5c840fc5f3d321382a6f2d83eff78cc7bec072c8
achyuthdasari <achyuthdasari.24@gmail.com>
2021-08-31 08:52:24 +0530

    solved

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -3,7 +3,19 @@
 
 
 
-def isprime
+def is_prime(n):
+	if n==2: return True
+	elif n==1 or n==0: return False
+	elif n%2==0: return False
+	else:
+		for i in range(3,int(n**0.5)+1,2):
+			if n%i==0: return False
+	return True
+
 def fun_hasnoprimes(l):
+	for i in l:
+		for j in i:
+			if is_prime(j):
+				return False
 	return True
 
